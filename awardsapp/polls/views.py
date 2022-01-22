1f97ebfa13700f6516aedcbab8e38e3004efb5a9
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-01-21 23:29:45 -0500

    Function views

--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello world!')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Question number {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Question results {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Vote for {question_id}')
