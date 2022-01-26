13742e6cb45b931809baa90479fa770882832978
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-01-25 21:39:58 -0500

    Class base views

--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -1,27 +1,38 @@
 """Polls views."""
 
 # Django
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 # Models
 from polls.models import Choice, Question
 
 
-def index(request):
-    questions = Question.objects.all()
-    return render(request, 'polls/index.html', {'questions': questions})
+class IndexView(generic.ListView):
+    """Handle the question list."""
+
+    template_name = 'polls/index.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    """Handle the question detail."""
+
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultView(generic.DetailView):
+    """Handle the question results."""
+
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
