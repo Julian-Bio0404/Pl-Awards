7bac7ff059b9b751ed94313256bb482b6d04ba0e
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-02-02 23:01:12 -0500

    Models admin

--- awardsapp/polls/admin.py
+++ awardsapp/polls/admin.py
@@ -1,10 +1,26 @@
 """Polls admin models."""
 
 # Django
+from re import search
 from django.contrib import admin
 
 # Models
 from .models import Choice, Question
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    """Choice Inline admin."""
+
+    model = Choice
+    extra = 3
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """Question model admin."""
+
+    fields = ['text', 'date']
+    inlines = [ChoiceInline]
+    list_display = ['text', 'date', 'published_recently']
+    list_filter = ['date']
+    search_fields = ['text']
